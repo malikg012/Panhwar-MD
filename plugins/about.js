2dee0cceb77f61999dad9217aaa5376e930bcf73
PROxANSAR <panhwarking003@gmail.com>
2024-12-07 21:12:02 +0500

    Update and rename about.js to plugins/about.js

--- plugins/about.js
+++ plugins/about.js
@@ -11,11 +11,11 @@ async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender
 try{
 let madeMenu = `❁ ════ ❃•⇆•❃ ════ ❁
 
-*👋 HELLO ${pushname}*\n\n *I AM SOBIA BUTT 746*\n\n *I AM 20 YEARS OLD Girl 🔞*\n\n *I AM FROM PERSNOL HAI😁🙈*\n\n *I AM SIMPLE WHATSAPP BOT DEVELOPER👨‍💻*\n\n\n *THANK YOU.😊*
+*👋 HELLO ${pushname}*\n\n *I AM PROxANSAR 746*\n\n *I AM 15 YEARS OLD BOY 🔞*\n\n *I AM FROM PERSNOL HAI😁🙈*\n\n *I AM SIMPLE WHATSAPP BOT DEVELOPER👨‍💻*\n\n\n *THANK YOU.😊*
 
 ❁ ════ ❃•⇆•❃ ════ ❁
 
-> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ SOBIA MD*
+> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ PANHWAR MD*
 
 ╰━❁ ═══ ❃•⇆•❃ ═══ ❁━╯
 `
