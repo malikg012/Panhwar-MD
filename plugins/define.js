386c96197444e61f3b9cf89f8e7f74583c407850
PROxANSAR <panhwarking003@gmail.com>
2024-12-08 07:41:54 +0500

    Update define.js

--- plugins/define.js
+++ plugins/define.js
@@ -28,7 +28,7 @@ const wordInfo = `
 📝 *Example*: ${example}
 🔗 *Synonyms*: ${synonyms}
 
-*MADE WITH ♥ SOBIA MD*`;
+*MADE WITH ♥ PANHWAR MD*`;
 
                                  return reply(wordInfo);
                              } catch (e) {
