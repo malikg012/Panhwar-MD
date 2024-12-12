b74184a74f3215aedbf2f28e5de1d0eedab2a3a1
PROxANSAR <panhwarking003@gmail.com>
2024-12-12 19:20:41 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || "enter your session",
-    CAPTION: process.env.CAPTION || "POWERED BY ANSAR-PANHWAR",
+    CAPTION: process.env.CAPTION || "ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴀɴsᴀʀ-ᴘᴀɴʜᴡᴀʀ",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
     MODE: process.env.MODE || "public",
     AUTO_VOICE: process.env.AUTO_VOICE || "true",
     AUTO_STICKER: process.env.AUTO_STICKER || "false",
     AUTO_REPLY: process.env.AUTO_REPLY || "false",
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://endpoint.web.id/server/file/hN0zwDDwcw66gisn.jpg",
-    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M PANHWAR-MD WHATSAPP BOT 😊♻️",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imghippo.com/files/VlzR4698Zv.jpg",
+    ALIVE_MSG: process.env.ALIVE_MSG || "𝐇𝐈𝐈 𝐃𝐄𝐀𝐑 𝐈𝐌 𝐎𝐍𝐋𝐈𝐍𝐄 𝐈'𝐌 𝐏𝐀𝐍𝐇𝐖𝐀𝐑-𝐌𝐃 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐁𝐎𝐓 😊♻️",
     ANTI_LINK: process.env.ANTI_LINK || "true",
     ANTI_BAD: process.env.ANTI_BAD || "true",
     PREFIX: process.env.PREFIX || ".",
@@ -23,6 +23,6 @@ module.exports = {
     AUTO_REACT: process.env.AUTO_REACT || "true",
     HEART_REACT: process.env.HEART_REACT || "true",
     OWNER_REACT: process.env.OWNER_REACT || "true",
-    BOT_NAME: process.env.BOT_NAME || "➺PANHWAR-MD࿐",
+    BOT_NAME: process.env.BOT_NAME || "➺ᴘᴀɴʜᴡᴀʀ-ᴍᴅ࿐",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
