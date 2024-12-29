bf6296fbd2d27846292ce84ce2edf0c2ac768f4e
PROxANSAR <panhwarking003@gmail.com>
2024-12-29 08:20:08 +0500

    💀🫀👑

--- plugins/listcmd.js
+++ plugins/listcmd.js
@@ -297,15 +297,15 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://qu.ax/QkpAj.jpg` },
+                image: { url: `https://i.ibb.co/59675LG/Manul-Ofc-X.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363369260614615@newsletter',
-                        newsletterName: 'AWAIS MD',
+                        newsletterJid: '120363321103874131@newsletter',
+                        newsletterName: 'ᴀɴsᴀʀ-ᴘᴀɴʜᴡᴀʀ',
                         serverMessageId: 143
                     }
                 }
